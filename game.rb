3280c99f8990a23f529b09e4f5648a76c779869a
sandeepkaurs <sandeep.singh@telus.com>
2023-01-26 03:03:32 -0800

    Commit: replaced if statement in game.rb with winner an check score code

--- game.rb
+++ game.rb
@@ -12,8 +12,8 @@ class Game
   # Statement to begin game and welcome both players
   def start
     puts "Lets Begin. Welcome #{player_1.name} and #{player_2.name}"
-    pp @player_1
-    pp @player_2
+    # pp @player_1
+    # pp @player_2
     turn
   end
   
@@ -22,17 +22,36 @@ class Game
     puts "P1: #{@player_1.lives}/3 vs P2: #{@player_2.lives}/3"
   end
 
+  def winner(player)
+    puts "#{player.name} wins with a score of #{player.lives}/3"
+    puts "-----GAME-OVER-----"
+    puts "Good bye!"
+    exit(0) 
+  end
+
+  def check_score
+    if @player_1.lives == 0
+      winner(player_2)
+    elsif @player_2.lives == 0
+      winner(player_1)
+    end  
+  end
+
   def turn
     @player_1.new_question
+    check_score
     @player_2.new_question
-    
-    if @player_1.lives > 0 && @player_2.lives > 0
-      show_stats
-      puts "-----NEW-TURN-----"
-      turn
-    else
-      puts "-----GAME-OVER-----"
-      puts "Good bye!"
-    end 
+    check_score
+    show_stats
+    puts "-----NEW-TURN-----"
+    turn
+    # if @player_1.lives > 0 && @player_2.lives > 0
+    #   show_stats
+    #   puts "-----NEW-TURN-----"
+    #   turn
+    # else
+    #   puts "-----GAME-OVER-----"
+    #   puts "Good bye!"
+    # end 
   end  
 end  
